e9b5fb76bb6b98c21b532a94a3efce6ddbc2b648
tanjonaaa <hei.tanjona@gmail.com>
2023-11-16 21:35:38 +0300

    feat: added generic interface for CRUD operations

---
+++ src/main/java/org/example/Crud/CrudOperations.java
@@ -0,0 +1,10 @@
+package org.example.Crud;
+
+import java.util.List;
+
+public interface CrudOperations<T>{
+    List<T> findAll();
+    List<T> saveAll(List<T> toSave);
+    T save(T toSave);
+    T delete(T toDelete);
+}
