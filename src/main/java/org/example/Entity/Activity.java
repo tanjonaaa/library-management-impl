6e23909e31c28b6dad884d944f9a6f502b93840b
tanjonaaa <hei.tanjona@gmail.com>
2023-11-18 11:39:09 +0300

    refactor: used lombok annotations for all models

--- src/main/java/org/example/Entity/Activity.java
+++ src/main/java/org/example/Entity/Activity.java
@@ -1,49 +1,17 @@
 package org.example.Entity;
 
+import lombok.*;
+
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Activity {
     private String id;
     private String SubscriberId;
     private String bookId;
     private LocalDateTime createdAt;
-
-    public Activity(String id, String subscriberId, String bookId, LocalDateTime createdAt) {
-        this.id = id;
-        SubscriberId = subscriberId;
-        this.bookId = bookId;
-        this.createdAt = createdAt;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getSubscriberId() {
-        return SubscriberId;
-    }
-
-    public void setSubscriberId(String subscriberId) {
-        SubscriberId = subscriberId;
-    }
-
-    public String getBookId() {
-        return bookId;
-    }
-
-    public void setBooks(String bookId) {
-        this.bookId = bookId;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
 }
